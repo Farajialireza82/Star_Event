bab1418ec04e7582ffa802624ed1203d49e8c2ee
Farajialireza82 <Farajialireza82@gmail.com>
2019-10-18 17:00:33 +0330

    fixed some typos

--- app/src/main/java/com/example/arc_exapmle/NoteDatabase.java
+++ app/src/main/java/com/example/arc_exapmle/NoteDatabase.java
@@ -9,7 +9,8 @@ import androidx.room.Room;
 import androidx.room.RoomDatabase;
 import androidx.sqlite.db.SupportSQLiteDatabase;
 
-@Database(entities = {Note.class}, version = 1 , exportSchema = false)
+@Database(entities = {Note.class}, version = 1, exportSchema = false)
+
 public abstract class NoteDatabase extends RoomDatabase {
 
     private static NoteDatabase instance;
@@ -28,7 +29,7 @@ public abstract class NoteDatabase extends RoomDatabase {
         return instance;
     }
 
-    private static RoomDatabase.Callback roomCallback = new RoomDatabase.Callback(){
+    private static RoomDatabase.Callback roomCallback = new RoomDatabase.Callback() {
 
         @Override
         public void onCreate(@NonNull SupportSQLiteDatabase db) {
@@ -39,19 +40,19 @@ public abstract class NoteDatabase extends RoomDatabase {
     };
 
 
-    private static class PopulateDbAsyncTask extends AsyncTask<Void , Void , Void>{
+    private static class PopulateDbAsyncTask extends AsyncTask<Void, Void, Void> {
         private NoteDao noteDao;
 
-        private PopulateDbAsyncTask(NoteDatabase db){
+        private PopulateDbAsyncTask(NoteDatabase db) {
             noteDao = db.noteDao();
         }
 
         @Override
         protected Void doInBackground(Void... voids) {
 
-            noteDao.insert(new Note("Title 1 " , "Description 1 " , 1));
-            noteDao.insert(new Note("Title 2 " , "Description 2 " , 2));
-            noteDao.insert(new Note("Title 3 " , "Description 3 " , 3));
+            noteDao.insert(new Note("Title 1 ", "Description 1 ", 1));
+            noteDao.insert(new Note("Title 2 ", "Description 2 ", 2));
+            noteDao.insert(new Note("Title 3 ", "Description 3 ", 3));
             return null;
         }
     }
