60e1211b1eca9c5a480d2b4d8712d7e5bd41a6eb
Farajialireza82 <Farajialireza82@gmail.com>
2019-10-19 13:57:33 +0330

    changed Parcelable extension to Serializable

--- app/src/main/java/com/example/arc_exapmle/NoteEntity.kt
+++ app/src/main/java/com/example/arc_exapmle/NoteEntity.kt
@@ -4,24 +4,18 @@ import android.os.Parcel
 import android.os.Parcelable
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import java.io.Serializable
 
 @Entity(tableName = "note_table")
 class NoteEntity(
      var title: String?,
      var description: String?,
      var priority: Int?
-) :  Parcelable {
+) :  Serializable {
 
     @PrimaryKey(autoGenerate = true)
     private var id = 0
 
-    constructor(parcel: Parcel) : this(
-        parcel.readString(),
-        parcel.readString(),
-        parcel.readValue(Int::class.java.classLoader) as? Int
-    ) {
-        id = parcel.readInt()
-    }
 
     fun setId(id : Int){
         this.id = id
@@ -31,28 +25,4 @@ class NoteEntity(
     fun getId(): Int {
         return id
     }
-
-
-    override fun writeToParcel(parcel: Parcel, flags: Int) {
-        parcel.writeString(title)
-        parcel.writeString(description)
-        parcel.writeValue(priority)
-        parcel.writeInt(id)
     }
-
-    override fun describeContents(): Int {
-        return 0
-    }
-
-    companion object CREATOR : Parcelable.Creator<NoteEntity> {
-        override fun createFromParcel(parcel: Parcel): NoteEntity {
-            return NoteEntity(parcel)
-        }
-
-        override fun newArray(size: Int): Array<NoteEntity?> {
-            return arrayOfNulls(size)
-        }
-    }
-
-
-}
