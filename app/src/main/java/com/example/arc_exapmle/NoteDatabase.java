8c4ae49f8ad2f8e601be76694431786a0f0d9986
Farajialireza82 <Farajialireza82@gmail.com>
2019-11-02 15:35:12 +0330

    made the code more beautiful

--- app/src/main/java/com/example/arc_exapmle/NoteDatabase.java
+++ app/src/main/java/com/example/arc_exapmle/NoteDatabase.java
@@ -52,7 +52,7 @@ public abstract class NoteDatabase extends RoomDatabase {
 
             noteDao.insert(new NoteEntity("Hint 1 ", "Touch the red button to add notes ", 1));
             noteDao.insert(new NoteEntity("Hint 2 ", "scroll right or left to delete them ", 2));
-            noteDao.insert(new NoteEntity("Hint 3 ", "Delete all notes with  3 ", 3));
+            noteDao.insert(new NoteEntity("Hint 3 ", "Delete all notes from the ActionBar ", 3));
             return null;
         }
     }
