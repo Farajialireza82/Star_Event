803f2c9451314c57e2d9059d20d25235397a41bc
Farajialireza82 <Farajialireza82@gmail.com>
2019-11-05 18:35:55 +0330

    changed String userID to Int UserID

--- app/src/main/java/com/example/arc_exapmle/note/NoteUI.kt
+++ app/src/main/java/com/example/arc_exapmle/note/NoteUI.kt
@@ -4,5 +4,5 @@ import android.os.Parcelable
 import kotlinx.android.parcel.Parcelize
 
 @Parcelize
-data class NoteUI(var id: Int, var title: String , var description: String , var priority: Int , var userID: String) :
+data class NoteUI(var id: Int, var title: String , var description: String , var priority: Int , var userID: Int) :
     Parcelable
