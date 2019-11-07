29f67df5a30e8466506a858ce173b4e6db71c2e3
Farajialireza82 <Farajialireza82@gmail.com>
2019-11-07 19:30:14 +0330

    putted each NoteEntity noteIds in their constructors

--- app/src/main/java/com/example/arc_exapmle/note/NoteDatabase.java
+++ app/src/main/java/com/example/arc_exapmle/note/NoteDatabase.java
@@ -9,7 +9,7 @@ import androidx.room.Room;
 import androidx.room.RoomDatabase;
 import androidx.sqlite.db.SupportSQLiteDatabase;
 
-@Database(entities = {NoteEntity.class}, version = 2, exportSchema = false)
+@Database(entities = {NoteEntity.class}, version = 2, exportSchema = false )
 
 public abstract class NoteDatabase extends RoomDatabase {
 
@@ -50,9 +50,9 @@ public abstract class NoteDatabase extends RoomDatabase {
         @Override
         protected Void doInBackground(Void... voids) {
 
-            noteDao.insert(new NoteEntity("Hint 1 ", "Touch the red button to add notes ", 1 , 82) );
-            noteDao.insert(new NoteEntity("Hint 2 ", "scroll right or left to delete them ", 2  , 82));
-            noteDao.insert(new NoteEntity("Hint 3 ", "Delete all notes from the ActionBar ", 3 , 82));
+            noteDao.insert(new NoteEntity("Hint 1 ", "Touch the red button to add notes ", 1 , 82 , 0) );
+            noteDao.insert(new NoteEntity("Hint 2 ", "scroll right or left to delete them ", 2  , 82 , 0 ));
+            noteDao.insert(new NoteEntity("Hint 3 ", "Delete all notes from the ActionBar ", 3 , 82 , 0));
             return null;
         }
     }
