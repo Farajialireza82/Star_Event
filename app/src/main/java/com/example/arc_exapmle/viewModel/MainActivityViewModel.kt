ae085e4207d9325160c1c4535d32ae68e691f109
Farajialireza82 <Farajialireza82@gmail.com>
2019-12-07 22:33:36 +0330

    created deleteNote & deleteAllNotes function

--- app/src/main/java/com/example/arc_exapmle/viewModel/MainActivityViewModel.kt
+++ app/src/main/java/com/example/arc_exapmle/viewModel/MainActivityViewModel.kt
@@ -1,12 +1,36 @@
 package com.example.arc_exapmle.viewModel
 
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.example.arc_exapmle.note.NoteEntity
+import com.example.arc_exapmle.note.NoteRepository
 import com.example.arc_exapmle.note.NoteUI
 
-class MainActivityViewModel : ViewModel() {
+class MainActivityViewModel(repository: NoteRepository) : ViewModel() {
 
-    val notesMutableLiveData :MutableLiveData<List<NoteUI>> = MutableLiveData()
+    private var repository = repository
+    val allNotes: LiveData<List<NoteEntity>> = repository.getAllNotes()
+
+    val toastMutableLiveData: MutableLiveData<String> = MutableLiveData()
+
+    fun deleteNote(noteUI: NoteUI) {
+
+        repository.delete(noteUI)
+
+        toastMutableLiveData.value = "Note deleted Successfully"
+
+    }
+
+    fun deleteAllNotes() {
+
+        repository.deleteAllNotes()
+
+        toastMutableLiveData.value = "All notes deleted"
+
+    }
 
 
 }
