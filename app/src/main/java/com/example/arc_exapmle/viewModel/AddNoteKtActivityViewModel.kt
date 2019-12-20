f4e54cb8b2e9761ccb520da1315a4a5ec883f137
Farajialireza82 <Farajialireza82@gmail.com>
2019-12-20 18:53:24 +0330

    Deleted unnecessary imports

--- app/src/main/java/com/example/arc_exapmle/viewModel/AddNoteKtActivityViewModel.kt
+++ app/src/main/java/com/example/arc_exapmle/viewModel/AddNoteKtActivityViewModel.kt
@@ -1,25 +1,29 @@
 package com.example.arc_exapmle.viewModel
 
-import android.content.ClipDescription
-import android.renderscript.RenderScript
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.example.arc_exapmle.note.NoteEntity
 import com.example.arc_exapmle.note.NoteRepository
 import com.example.arc_exapmle.note.NoteUI
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers.Default
+import kotlinx.coroutines.Dispatchers.IO
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.launch
+import kotlin.random.Random
 
-class AddNoteKtActivityViewModel (noteRepository: NoteRepository) : ViewModel(){
+class AddNoteKtActivityViewModel(noteRepository: NoteRepository) : ViewModel() {
 
     private val noteRepository = noteRepository
 
-    val titleMutableLiveData : MutableLiveData<String> = MutableLiveData()
-    val descriptionMutableLiveData : MutableLiveData<String> = MutableLiveData()
-    val toastMutableLiveData : MutableLiveData<String> = MutableLiveData()
+    val titleMutableLiveData: MutableLiveData<String> = MutableLiveData()
+    val descriptionMutableLiveData: MutableLiveData<String> = MutableLiveData()
+    val toastMutableLiveData: MutableLiveData<String> = MutableLiveData()
 
-    val onSuccessMutableLiveData : MutableLiveData<Boolean> = MutableLiveData()
+    val onSuccessMutableLiveData: MutableLiveData<Boolean> = MutableLiveData()
 
 
-    fun addNote(title :String , description :String , priority: Int){
+    fun addNote(title: String, description: String, priority: Int) {
 
 
         when {
@@ -38,9 +42,21 @@ class AddNoteKtActivityViewModel (noteRepository: NoteRepository) : ViewModel(){
 
             }
             else -> {
-                val noteEntity = NoteEntity(title , description , priority , noteRepository.userId )
+                val noteEntity = NoteEntity(title, description, priority, noteRepository.userId)
 
-                noteRepository.insert(NoteUI(noteEntity.noteId , noteEntity.title , noteEntity.description , noteEntity.priority , noteEntity.userId))
+
+                    noteRepository.insert(
+                        NoteUI(
+                            noteEntity.noteId,
+                            noteEntity.title,
+                            noteEntity.description,
+                            noteEntity.priority,
+                            noteEntity.userId
+                        )
+                    )
+
+
+
 
                 toastMutableLiveData.value = "Note Added Successfully "
 
@@ -50,7 +66,6 @@ class AddNoteKtActivityViewModel (noteRepository: NoteRepository) : ViewModel(){
 
 
     }
-
 
 
 }
