67000ed66c943fdf5848a2bc5254c7b5070f3caa
Farajialireza82 <Farajialireza82@gmail.com>
2019-12-25 14:49:54 +0330

    made update & deleteAll function suspended

--- app/src/main/java/com/example/arc_exapmle/note/NoteDao.kt
+++ app/src/main/java/com/example/arc_exapmle/note/NoteDao.kt
@@ -10,19 +10,19 @@ import androidx.room.*
     suspend fun insert(note: NoteEntity)
 
     @Update
-    fun update(note: NoteEntity)
+    suspend fun update(note: NoteEntity)
 
     @Delete
-    fun delete(note: NoteEntity)
+    suspend fun delete(note: NoteEntity)
 
     @Query("DELETE FROM note_table")
-    fun deleteAll()
+    suspend fun deleteAll()
 
     @Query("SELECT * FROM note_table WHERE user = :userID")
-     fun getAllNotes(userID: Int): LiveData<List<NoteEntity>>
+    fun getAllNotes(userID: Int): LiveData<List<NoteEntity>>
 
    @Query("SELECT * FROM note_table")
-   fun getEveryNoteThereIs(): LiveData<List<NoteEntity>>
+    fun getEveryNoteThereIs(): LiveData<List<NoteEntity>>
 
 
 
