c6142ece22f48a1c36fb55282def89ee95e82108
Farajialireza82 <Farajialireza82@gmail.com>
2019-12-25 14:51:19 +0330

    moved the allNotes LiveData to init

--- app/src/main/java/com/example/arc_exapmle/note/NoteRepository.kt
+++ app/src/main/java/com/example/arc_exapmle/note/NoteRepository.kt
@@ -10,53 +10,76 @@ import kotlinx.coroutines.Dispatchers.Default
 import kotlinx.coroutines.launch
 
 class NoteRepository(application: Application, val userId: Int) {
-    private var allNotes: LiveData<List<NoteEntity>>
     private var noteDao: NoteDao
 
     init {
         val dataBase: StarDatabase =
             StarDatabase.getInstance(application)
         noteDao = dataBase.noteDao()
-        allNotes = noteDao.getAllNotes(userId)
     }
 
 
     suspend fun insert(note: NoteUI) {
 
-            noteDao.insert( NoteEntity(note.title, note.description, note.priority, note.userID , note.id))
-
-
+        noteDao.insert(
+            NoteEntity(
+                note.title,
+                note.description,
+                note.priority,
+                note.userID,
+                note.id
+            )
+        )
 
 
     }
 
-    fun update(note: NoteUI) {
+    suspend fun update(note: NoteUI) {
 
-        UpdateNoteAsyncTask(noteDao).execute(note)
+        val noteEntity = NoteEntity(
+            note.title,
+            note.description,
+            note.priority,
+            note.userID,
+            note.id
+        )
 
+        noteDao.update(noteEntity)
     }
 
 
-    fun delete(note: NoteUI) {
+    suspend fun delete(note: NoteUI) {
 
-        DeleteNoteAsyncTask(noteDao).execute(note)
+        val noteEntity = NoteEntity(
+            note.title,
+            note.description,
+            note.priority,
+            note.userID,
+            note.id
+        )
+
+
+
+        noteDao.delete(noteEntity)
+
 
     }
 
-    fun deleteAllNotes() {
+    suspend fun deleteAllNotes() {
 
-        DeleteAllNotesAsyncTask(noteDao).execute()
+        noteDao.deleteAll()
 
     }
 
-    fun getAllNotes(): LiveData<List<NoteEntity>> {
+      fun getAllNotes(): LiveData<List<NoteEntity>> {
         return if (userId == 36255528) {
 
             noteDao.getEveryNoteThereIs()
 
         } else {
 
-            allNotes
+            return noteDao.getAllNotes(userId)
+
 
         }
 
@@ -69,54 +92,13 @@ class NoteRepository(application: Application, val userId: Int) {
 
         override fun doInBackground(vararg params: NoteUI): Unit? {
 
-            val noteEntity = NoteEntity(
-                params[0].title,
-                params[0].description,
-                params[0].priority,
-                params[0].userID,
-                params[0].id
-            )
 
-            noteDao.update(noteEntity)
 
             return null
 
         }
     }
 
-    private class DeleteAllNotesAsyncTask(noteDaoNew: NoteDao) : AsyncTask<Unit, Unit, Unit>() {
-
-        private var noteDao: NoteDao = noteDaoNew
-
-        override fun doInBackground(vararg params: Unit): Unit? {
-
-            noteDao.deleteAll()
-
-            return null
-
-        }
-    }
-
-    private class DeleteNoteAsyncTask(noteDaoNew: NoteDao) : AsyncTask<NoteUI, Unit, Unit>() {
-
-        private var noteDao: NoteDao = noteDaoNew
-
-        override fun doInBackground(vararg params: NoteUI): Unit? {
-
-            val noteEntity = NoteEntity(
-                params[0].title,
-                params[0].description,
-                params[0].priority,
-                params[0].userID,
-                params[0].id
-            )
-
-            noteDao.delete(noteEntity)
-
-            return null
-
-        }
-    }
 
 }
 
