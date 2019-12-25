637706d1699564842532f4a8b8fda33960224bda
Farajialireza82 <Farajialireza82@gmail.com>
2019-12-25 14:50:41 +0330

    deleted some unused lines of code

--- app/src/main/java/com/example/arc_exapmle/note/NoteEntity.kt
+++ app/src/main/java/com/example/arc_exapmle/note/NoteEntity.kt
@@ -7,37 +7,7 @@ import androidx.room.PrimaryKey
 import com.example.arc_exapmle.user.UserEntity
 import java.io.Serializable
 
-/*
-@Entity(
-    tableName = "note_table",
-            foreignKeys = [ForeignKey(
-                entity = UserEntity::class,
-                parentColumns = arrayOf("id"),
-                childColumns = arrayOf("user_id"))]
-)
-class NoteEntity(
-    var title: String,
-    var description: String,
-    var priority: Int,
-    @ColumnInfo(name = "user_id" )
-    var userId:Int
-    ) : Serializable {
 
-    @PrimaryKey(autoGenerate = true)
-    @ColumnInfo(name = "note_id")
-    private var noteId = 0
-
-    fun setNoteId(note_id: Int) {
-        this.noteId = note_id
-    }
-
-    fun getNoteId(): Int {
-        return noteId
-    }
-
-
-}
-*/
 
 @Entity(
     tableName = "note_table",
